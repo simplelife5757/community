b9597b364df94ba90c26956e53c022cba87f6311
Jiwoo Kim <jiwoo627@naver.com>
2019-07-29 21:12:00 +0900

    [#6] Add Post#update() and delete duplicated `writer` field

--- mother-api/src/main/java/community/mother/domain/post/domain/Post.java
+++ mother-api/src/main/java/community/mother/domain/post/domain/Post.java
@@ -2,7 +2,9 @@ package community.mother.domain.post.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import community.mother.domain.account.domain.Account;
+import community.mother.domain.post.dto.request.UpdatePostParams;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,14 +18,12 @@ import java.time.LocalDateTime;
 @Table(name = "post")
 @Entity
 @NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Post {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "POST_ID")
 	private Long id;
-
-	@Column(nullable = false)
-	private String writer;
 
 	@Column(nullable = false)
 	private String title;
@@ -34,7 +34,7 @@ public class Post {
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@ManyToOne
 	@JoinColumn(name = "account_id", nullable = false, updatable = false)
-	private Account account;
+	private Account writer;
 
 	@CreationTimestamp
 	@Column(nullable = false, updatable = false)
@@ -43,31 +43,36 @@ public class Post {
 	@UpdateTimestamp
 	private LocalDateTime updateddAt;
 
-	@Column(nullable = false)
-	private int likeCount;
+	private Long likeCount;
 
-	@Column(nullable = false)
-	private int viewCount;
+	private Long viewCount;
 
 	private String originalFileName;
 
 	private String savedFileName;
 
 	@Builder
-	private Post(String writer,
-				 String title,
+	private Post(String title,
 				 String content,
+				 Account writer,
 				 String originalFileName,
 				 String savedFileName
 	) {
-		Assert.hasLength(writer, "writer should not be empty.");
 		Assert.hasLength(title, "title should not be empty.");
 		Assert.hasLength(content, "content should not be empty.");
 
-		this.writer = writer;
 		this.title = title;
 		this.content = content;
+		this.writer = writer;
 		this.originalFileName = originalFileName;
 		this.savedFileName = savedFileName;
+	}
+
+	public void update(Post postToUpdate) {
+		this.content = postToUpdate.getContent();
+	}
+
+	public boolean isWriter(Long accountId) {
+		return this.writer.getId().equals(accountId);
 	}
 }
