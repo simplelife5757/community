63dc7b5902e8909b67a90a390ca80fb00582ea1b
jiwoo.kim <jiwoo627@naver.com>
2019-08-15 03:16:55 +0900

    [#tutorial] reafctor

--- mother-api/src/main/java/community/mother/domain/account/security/SecurityConfig.java
+++ mother-api/src/main/java/community/mother/domain/account/security/SecurityConfig.java
@@ -54,25 +54,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
 
-//	아래보다 위에가 서버를 덜 괴롭혀서 더 효율적
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.anonymous() //익명사용자만 사용 가능
-//				.and()
-//			.formLogin() //폼 인증 사용
-//				.and()
-//			.authorizeRequests() //허용할 요청
-//			.mvcMatchers(HttpMethod.GET, "/api/**").permitAll()
-//			.anyRequest().authenticated(); //나머지는 인증 필요
-
-//		http.httpBasic().disable()
-//				.csrf().disable()
-//				.headers().frameOptions().disable();
-//	}
-
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
 }
